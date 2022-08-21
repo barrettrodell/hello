a072daf2e8f5c82598dd80d7a5bc86d6d26ce8cb
Barrett Rodell <brodell2@huskers.unl.edu>
2022-08-21 19:37:20 +0000

    added my major to the output and corrected issues with code

--- hello.c
+++ hello.c
@@ -1,1 +1,18 @@
-printf("Barrett Rodell, Computer Science\n");
+/**
+ * Author: Barrett Rodell
+ *         brodell2@huskers.unl.edu
+ * Date: 2022/08/21
+ *
+ * A simple hello world program in C for Hack 1.0
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Barrett Rodell\n");
+  printf("Computer Science\n");
+
+  return 0;
+}
