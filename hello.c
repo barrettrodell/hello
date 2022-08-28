94d1afdc9e618023bb56963d012ec09fc64eebb0
Preston Pinkelman <ppinkelman2@huskers.unl.edu>
2022-08-28 20:34:58 +0000

    Added partner information

--- hello.c
+++ hello.c
@@ -13,6 +13,8 @@ int main(int argc, char **argv) {
 
   printf("Barrett Rodell\n");
   printf("Computer Science\n");
+  printf("Partner: Preston Pinkelman\n");
+  printf("Computer Science\n");
 
   return 0;
 }
